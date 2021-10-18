294ea9f4c409406d299af6d140771145784c104c
Bruno Winkeler <bruno.winkeler@virtus.ufcg.edu.br>
2021-10-18 19:35:51 -0300

    Add form and inputs
    - Separate counter example.

--- src/app.js
+++ src/app.js
@@ -3,51 +3,49 @@ console.log('App.js is running!');
 const app = {
   title: 'Indecision App',
   subtitle: 'Put your life in the hands of a computer',
-  options: ['One', 'Two']
+  options: []
 };
 
-const template = (
-  <div>
-    <h1>{app.title}</h1>
-    {(app.subtitle) && <p>{app.subtitle}</p>}
-    <p>{(app.options.length > 0) ? 'Here are your options:' : 'No options'}</p>
-    <ol>
-      <li>Item One</li>
-      <li>Item Two</li>
-    </ol>
-  </div>
-);
+const onFormSubmit = (e) => {
+  e.preventDefault();
 
-let count = 0;
+  const option = e.target.elements.option.value;
 
-const addOne = () => {
-  count++;
-  renderCounterApp();
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+  }
+
+  renderApp();
 };
 
-const minusOne = () => {
-  count--;
-  renderCounterApp();
-};
-
-const reset = () => {
-  count = 0;
-  renderCounterApp();
+const removeAllElements = () => {
+  app.options = [];
+  renderApp();
 };
 
 const appRoot = document.getElementById('app');
 
-const renderCounterApp = () => {
-  const templateTwo = (
+const renderApp = () => {
+  const template = (
     <div>
-      <h1>Count: {count}</h1>
-      <button onClick={addOne}>+1</button>
-      <button onClick={minusOne}>-1</button>
-      <button onClick={reset}>reset</button>
+      <h1>{app.title}</h1>
+      {(app.subtitle) && <p>{app.subtitle}</p>}
+      <p>{(app.options.length > 0) ? 'Here are your options:' : 'No options'}</p>
+      <p>{app.options.length}</p>
+      <button onClick={removeAllElements}>Remove All</button>
+      <ol>
+        <li>Item One</li>
+        <li>Item Two</li>
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add Option</button>
+      </form>
     </div>
   );
 
-  ReactDOM.render(templateTwo, appRoot);
-}
+  ReactDOM.render(template, appRoot);
+};
 
-renderCounterApp();
+renderApp();
