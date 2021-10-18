452a16f885f6f687089bcf2dda198dd855ce3ec7
Bruno Winkeler <bruno.winkeler@virtus.ufcg.edu.br>
2021-10-18 17:26:31 -0300

    Add arrow functions examples (01/02)

---
+++ src/playground/es6-arrow-function.js
@@ -0,0 +1,26 @@
+// // ES5 function
+// function square(x) {
+//   return x * x;
+// };
+
+// // Arrow function - OPTION 1
+// const squareArrow = (x) => {
+//   return x * x;
+// };
+
+// // Arrow function - OPTION 2
+// const squareArrow = (x) => x * x;
+
+// console.log(square(3));
+// console.log(squareArrow(8));
+
+// Example 2 - Challenge
+
+const getFirstName = (str) => {
+  return str.split(' ')[0];
+};
+
+const getFirstName2 = (str) => str.split(' ')[0];
+
+console.log(getFirstName('Bruno Winkeler'));
+console.log(getFirstName2('Raquel Winkeler'));
