4c12c560068fc5e2146d677b28626f7a8bab43ab
Bruno Winkeler <brunowinkeler@gmail.com>
2021-10-18 00:22:29 -0300

    Replace 'var' by 'const'

---
+++ src/playground/es6-let-const.js
@@ -0,0 +1,13 @@
+var nameVar = 'Bruno';
+var nameVar = 'Mike';
+console.log('namvar', nameVar);
+
+let nameLet = 'Jen';
+nameLet = 'Julie';
+console.log('nameLet', nameLet);
+
+const nameConst = 'Frank';
+console.log('nameConst', nameConst);
+
+// var is function-block-scoped
+// let and const are any-block-scoped
