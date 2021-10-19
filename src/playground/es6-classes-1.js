28eea4f17722b5a3eefb4013dd4c70d5daf0a35a
Bruno Winkeler <bruno.winkeler@virtus.ufcg.edu.br>
2021-10-19 17:18:26 -0300

    Add classes examples (01/02)
    - Inheritance

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -15,10 +15,53 @@ class Person {
   }
 }
 
-const me = new Person('Bruno Winkeler', 31);
-console.log(me.getGreeting());
+class Student extends Person {
+  constructor(name, age, major) {
+    super(name, age);
+    this.major = major;
+  }
+
+  hasMajor() {
+    return !!this.major;
+  }
+
+  getDescription() {
+    let description = super.getDescription();
+
+    if (this.hasMajor()) {
+      description += ` Their major is ${this.major}`;
+    }
+
+    return description;
+  }
+}
+
+
+class Traveler extends Person {
+  constructor(name, age, homeLocation) {
+    super(name, age);
+    this.homeLocation = homeLocation;
+  }
+
+  getGreeting() {
+    let greeting = super.getGreeting();
+
+    if (this.homeLocation) {
+      greeting += ` I am visiting from ${this.homeLocation}`;
+    }
+
+    return greeting;
+  }
+}
+
+const me = new Student('Bruno Winkeler', 31, 'Electrical Engineering');
 console.log(me.getDescription());
 
-const other = new Person();
-console.log(other.getGreeting());
+const other = new Student();
 console.log(other.getDescription());
+
+const trav = new Traveler('Bruno Winkeler', 31, 'João Pessoa');
+console.log(trav.getGreeting());
+
+const trav2 = new Traveler(undefined, undefined, 'Nowhere');
+console.log(trav2.getGreeting());
